1de36a06c2d5cdd37ab55b7d5505f2be626d14c3
Michael Eva <evamichael100@gmail.com>
2025-07-31 16:20:52 +0800

    Mark deletion of /app/Component.js

---
+++ DELETED__app_Component.js
@@ -0,0 +1,4 @@
+This file marks the deletion of: /app/Component.js
+Deleted by Simple Agent as part of directory restructuring.
+Original file path: /app/Component.js
+Deletion reason: Redundant directory removal
